dd4daa148712d9edc4f9d116b7a91dbcfc12c4cb
Wardah <wlt_93@hotmail.com>
2019-05-09 17:03:53 +0100

    Added feature test for Sulfuras

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,5 +7,14 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].name).to eq 'foo'
     end
+
+    it "checks for Sulfuras's quality not changing with days" do
+      item1 = [Item.new('Sulfuras, Hand of Ragnaros',0,80)]
+      item2 = [Item.new('Sulfuras, Hand of Ragnaros',-1,80)]
+      GildedRose.new(item1).update_quality
+      GildedRose.new(item2).update_quality
+      expect(item1[0].quality).to eq 80
+      expect(item2[0].quality).to eq 80
+    end
   end
 end
